4d31e4ae81d8f106ba21269b366174ba69189dea
Arielle2401000261 <muhorakeyeakanyanaarielle@gmail.com @gmail.com>
2025-04-12 18:16:16 +0200

    Initial commit

---
+++ site.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+    <head>
+        <title> welcome to UOK </title>
+</head>
+<body>
+    <?php
+    echo " <h1> our first php</h1>\n"
+    echo " this second line paragraphy\n";
+    echo " there once was a man named john\n";
+    echo "john was 35 years old\n";
+    ?>
+    </body>
+    </html>   
